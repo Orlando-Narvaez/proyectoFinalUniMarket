d13eab92467ee68341f90ffcd3af6593eb09de24
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-03-05 16:32:41 -0500

    Create Guarantee.java

---
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/model/Guarantee.java
@@ -0,0 +1,15 @@
+package co.edu.uniquindio.unimarket.model;
+
+import java.util.Date;
+
+public class Guarantee
+{
+    private String idGuarantee;
+    private Date dataRequest;
+    private Date dateDeliver;
+    private String idUser;
+    private String idProduct;
+
+    public Guarantee() {
+    }
+}
