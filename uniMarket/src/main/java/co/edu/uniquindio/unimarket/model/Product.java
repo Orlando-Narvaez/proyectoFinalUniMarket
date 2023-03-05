b39568c169b461b3bc6486624282a88dde12b9a4
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-03-05 16:32:55 -0500

    Create Product.java

---
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/model/Product.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.unimarket.model;
+
+import java.util.Date;
+
+public class Product
+{
+    private String idProduct;
+    private String name;
+    private float value;
+    private Date publicationDate;
+    private String description;
+    private String idImage;
+}
