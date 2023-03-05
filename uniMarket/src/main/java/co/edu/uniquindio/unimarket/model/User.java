34dacbc5cd6b8a667dd59a8cb01f91ea0eb294ee
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-03-05 16:33:14 -0500

    Create User.java

---
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/model/User.java
@@ -0,0 +1,11 @@
+package co.edu.uniquindio.unimarket.model;
+
+import java.util.Date;
+
+public class User
+{
+    private String idUser;
+    private Date creationDate;
+    private String userName;
+    private String idCard;
+}
