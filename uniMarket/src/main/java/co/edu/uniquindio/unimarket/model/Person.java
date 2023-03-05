f28b70563cf08d76831c7f1be3b8369253e58499
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-03-05 16:32:51 -0500

    Create Person.java

---
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/model/Person.java
@@ -0,0 +1,17 @@
+package co.edu.uniquindio.unimarket.model;
+
+import java.util.Date;
+
+public class Person
+{
+    private String idCard;
+    private String name1;
+    private String name2;
+    private String lastName1;
+    private String LastName2;
+    private Date birthDate;
+    private String numPhone;
+    private String email;
+    private String password;
+    private String address;
+}
