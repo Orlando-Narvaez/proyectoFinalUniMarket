57c82e113dcb8ba47ec300592f670391ca48ab06
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-03-06 22:46:16 -0500

    Update Cart.java
    Se parametriza y crean los atributos de la entidad Cart

--- uniMarket/src/main/java/co/edu/uniquindio/unimarket/model/Cart.java
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/model/Cart.java
@@ -1,6 +1,30 @@
 package co.edu.uniquindio.unimarket.model;
 
-public class Cart
-{
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
 
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Cart implements Serializable
+{
+    @Id
+    @EqualsAndHashCode.Include
+    @Column(name = "idCart",nullable = false, length = 20)
+    private String idCart;
+
+    private Date startDate;
+
+    private Date endingDate;
+
+    @Column(name = "idUser",nullable = false, length = 20)
+    private String idUser;
 }
