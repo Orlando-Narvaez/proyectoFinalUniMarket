7eedd2652a39337f4370698d4509424680ff882d
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-03-06 22:39:55 -0500

    Update Moderator.java
    Se parametriza los atributos de la entidad Moderator

--- uniMarket/src/main/java/co/edu/uniquindio/unimarket/model/Moderator.java
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/model/Moderator.java
@@ -1,8 +1,27 @@
 package co.edu.uniquindio.unimarket.model;
 
-public class Moderator
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+import java.io.Serializable;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Moderator implements Serializable
 {
+    @Id
+    @EqualsAndHashCode.Include
+    @Column(name = "idMod",nullable = false, length = 20)
     private String idMod;
+
+    @Column(name = "userName",nullable = false, length = 20, unique = true)
     private String userName;
+
+    @Column(name = "idCard",nullable = false, length = 20)
     private String idCard;
 }
