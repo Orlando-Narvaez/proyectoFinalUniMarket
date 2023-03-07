d1e284ba1dc8c145a19c3a6edf244dbd9977e5b1
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-03-06 22:39:41 -0500

    Update User.java
    Se parametriza los atributos de la entidad User

--- uniMarket/src/main/java/co/edu/uniquindio/unimarket/model/User.java
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/model/User.java
@@ -1,11 +1,29 @@
 package co.edu.uniquindio.unimarket.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+import java.io.Serializable;
 import java.util.Date;
 
-public class User
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class User implements Serializable
 {
+    @Id
+    @EqualsAndHashCode.Include
+    @Column(name = "idUser",nullable = false, length = 20)
     private String idUser;
     private Date creationDate;
+
+    @Column(name = "userName",nullable = false, length = 20, unique = true)
     private String userName;
+
+    @Column(name = "idCard",nullable = false, length = 20)
     private String idCard;
 }
