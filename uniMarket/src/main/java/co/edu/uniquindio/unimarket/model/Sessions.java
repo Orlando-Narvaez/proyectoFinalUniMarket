28f33e3d3c4db1d869c56b17930d45c7b064c1fd
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-03-06 22:42:06 -0500

    Update Sessions.java
    Se parametriza los atributos de la entidad Sessions

--- uniMarket/src/main/java/co/edu/uniquindio/unimarket/model/Sessions.java
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/model/Sessions.java
@@ -1,5 +1,6 @@
 package co.edu.uniquindio.unimarket.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.*;
@@ -16,8 +17,12 @@ import java.util.Date;
 public class Sessions implements Serializable
 {
     @Id
+    @EqualsAndHashCode.Include
+    @Column(name = "idSessions",nullable = false, length = 20)
     private String idSessions;
     private Date startDate;
     private Date endingDate;
+
+    @Column(name = "idUser",nullable = false, length = 20)
     private String idUser;
 }
