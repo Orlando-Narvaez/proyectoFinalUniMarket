9629407e0d001c3ad72637d485ba2b79c61e87df
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-03-06 22:39:31 -0500

    Update Person.java
    Se parametriza los atributos de la entidad Person

--- uniMarket/src/main/java/co/edu/uniquindio/unimarket/model/Person.java
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/model/Person.java
@@ -1,17 +1,49 @@
 package co.edu.uniquindio.unimarket.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+import java.io.Serializable;
 import java.util.Date;
+import java.util.Map;
 
-public class Person
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Person implements Serializable
 {
+    @Id
+    @EqualsAndHashCode.Include
+    @Column(name = "idCard",nullable = false, length = 20)
     private String idCard;
+
+    @Column(name = "name1",nullable = false, length = 20)
     private String name1;
+
+    @Column(name = "name2",nullable = false, length = 20)
     private String name2;
+
+    @Column(name = "lastName1",nullable = false, length = 20)
     private String lastName1;
+
+    @Column(name = "lastName2",nullable = false, length = 20)
     private String LastName2;
     private Date birthDate;
+
+    @Column(name = "numPhone",nullable = false, length = 20, unique = true)
     private String numPhone;
+
+    @Column(name = "email",nullable = false, length = 40, unique = true)
     private String email;
+
+    @Column(name = "password",nullable = false, length = 20)
     private String password;
-    private String address;
+
+    @ElementCollection
+    private Map<String, String> address;
 }
