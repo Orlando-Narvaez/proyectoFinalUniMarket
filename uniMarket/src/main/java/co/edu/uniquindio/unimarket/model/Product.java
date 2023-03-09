a00ef2f1cd9a662d97d2ff1d44ef05342dc7290b
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-03-08 22:16:14 -0500

    Update Product.java
    Se parametriza los atributos de la entidad Product

--- uniMarket/src/main/java/co/edu/uniquindio/unimarket/model/Product.java
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/model/Product.java
@@ -1,13 +1,41 @@
 package co.edu.uniquindio.unimarket.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.io.Serializable;
 import java.util.Date;
 
-public class Product
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Product implements Serializable
 {
+    @Id
+    @EqualsAndHashCode.Include
+    @Column(name = "idProduct", length = 20)
     private String idProduct;
+
+    @Column(name = "name", nullable = false, length = 20)
     private String name;
+
+    @Column(name = "value", nullable = false)
     private float value;
+
+    @Column(name = "publicationDate", nullable = false)
     private Date publicationDate;
+
+    @Column(name = "limitedDate", nullable = false)
+    private Date limitedDate;
+
+    @Column(name = "name", nullable = false)
     private String description;
-    private String idImage;
+
+    @Column(name = "state", nullable = false)
+    private Boolean state;
 }
