6af9d71565878edd56abffb191191949cccf7cb2
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-03-08 22:31:32 -0500

    Update User.java
    Se corrige el tipo de dato de la llave primaria

--- uniMarket/src/main/java/co/edu/uniquindio/unimarket/model/User.java
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/model/User.java
@@ -2,7 +2,6 @@ package co.edu.uniquindio.unimarket.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
 import lombok.*;
 import java.io.Serializable;
 import java.util.Date;
@@ -14,8 +13,8 @@ import java.util.Date;
 @ToString
 public class User extends Person implements Serializable
 {
-    @Column(name = "idCard",nullable = false, length = 20)
-    private String idCard;
+    @Column(name = "idCard",nullable = false)
+    private int idCard;
 
     private Date creationDate;
 }
