7465007021dcc7bc8f3f321634f2e770ca57df9c
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-03-08 22:31:40 -0500

    Update Product.java
    Se corrige el tipo de dato de la llave primaria

--- uniMarket/src/main/java/co/edu/uniquindio/unimarket/model/Product.java
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/model/Product.java
@@ -18,8 +18,7 @@ public class Product implements Serializable
 {
     @Id
     @EqualsAndHashCode.Include
-    @Column(name = "idProduct", length = 20)
-    private String idProduct;
+    private int idProduct;
 
     @Column(name = "name", nullable = false, length = 20)
     private String name;
