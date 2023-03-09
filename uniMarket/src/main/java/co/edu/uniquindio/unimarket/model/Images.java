0d47c35828a727d1a0b7107009bc6e32a9d040b9
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-03-08 22:32:15 -0500

    Update Images.java
    Se parametriza los atributos de la entidad Images

--- uniMarket/src/main/java/co/edu/uniquindio/unimarket/model/Images.java
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/model/Images.java
@@ -1,8 +1,28 @@
 package co.edu.uniquindio.unimarket.model;
 
-public class Images
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.io.Serializable;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Images implements Serializable
 {
-    private String idImage;
+    @Id
+    @EqualsAndHashCode.Include
+    private int idImage;
+
+    @Column(name = "nameImage", nullable = false, length = 30)
     private String nameImage;
+
+    @Column(name = "urlImage", nullable = false)
     private String urlImage;
 }
