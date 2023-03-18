addaaa5b597dfeff831b7c56d0d380d098e18b17
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-03-17 23:55:32 -0500

    Update Person.java
    Se cambia los datos "Date" por "LocalDateTime" en la clase Person

--- uniMarket/src/main/java/co/edu/uniquindio/unimarket/model/Person.java
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/model/Person.java
@@ -3,7 +3,7 @@ package co.edu.uniquindio.unimarket.model;
 import jakarta.persistence.*;
 import lombok.*;
 import java.io.Serializable;
-import java.util.Date;
+import java.time.LocalDateTime;
 
 @MappedSuperclass
 @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
@@ -18,28 +18,33 @@ public class Person implements Serializable
     @EqualsAndHashCode.Include
     private int idCard;
 
-    @Column(name = "name1",nullable = false, length = 20)
+    @Column(name = "name1", nullable = false, length = 20)
     private String name1;
 
-    @Column(name = "name2",nullable = false, length = 20)
+    @Column(name = "name2", nullable = false, length = 20)
     private String name2;
 
-    @Column(name = "lastName1",nullable = false, length = 20)
+    @Column(name = "lastName1", nullable = false, length = 20)
     private String lastName1;
 
-    @Column(name = "lastName2",nullable = false, length = 20)
+    @Column(name = "lastName2", nullable = false, length = 20)
     private String LastName2;
-    private Date birthDate;
 
-    @Column(name = "numPhone",nullable = false, length = 20, unique = true)
+    @Column(name = "birthDate", nullable = false)
+    private LocalDateTime birthDate;
+
+    @Column(name = "address", nullable = false, length = 30)
+    private String address;
+
+    @Column(name = "numPhone", nullable = false, length = 20, unique = true)
     private String numPhone;
 
-    @Column(name = "userName",nullable = false, length = 20, unique = true)
+    @Column(name = "userName", nullable = false, length = 20, unique = true)
     private String userName;
 
-    @Column(name = "email",nullable = false, length = 40, unique = true)
+    @Column(name = "email", nullable = false, length = 40, unique = true)
     private String email;
 
-    @Column(name = "password",nullable = false, length = 20)
+    @Column(name = "password", nullable = false, length = 20)
     private String password;
 }
