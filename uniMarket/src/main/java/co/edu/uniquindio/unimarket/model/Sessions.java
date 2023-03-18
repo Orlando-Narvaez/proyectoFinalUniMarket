c53668900c37f7594d125ff9ec90f8bd03eb8084
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-03-18 00:51:37 -0500

    Update Sessions.java
    Se agregan llaves foráneas

--- uniMarket/src/main/java/co/edu/uniquindio/unimarket/model/Sessions.java
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/model/Sessions.java
@@ -1,11 +1,10 @@
 package co.edu.uniquindio.unimarket.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Getter
@@ -25,4 +24,7 @@ public class Sessions implements Serializable
 
     @Column(name = "endingDate", nullable = false)
     private LocalDateTime endingDate;
+
+    @ManyToOne
+    private User user;
 }
