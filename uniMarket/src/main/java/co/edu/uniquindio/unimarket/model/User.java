a2e7f5c70e29b95acee362815e32364304c30092
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-03-18 00:51:43 -0500

    Update User.java
    Se agregan llaves foráneas

--- uniMarket/src/main/java/co/edu/uniquindio/unimarket/model/User.java
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/model/User.java
@@ -2,9 +2,12 @@ package co.edu.uniquindio.unimarket.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.*;
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Getter
@@ -18,4 +21,16 @@ public class User extends Person implements Serializable
 
     @Column(name = "creationDate",nullable = false)
     private LocalDateTime creationDate;
+
+    @ToString.Exclude
+    @OneToMany(mappedBy = "user")
+    private List<Sessions> sessionsList;
+
+    @ToString.Exclude
+    @OneToMany(mappedBy = "user")
+    private List<PQR> pqrList;
+
+    @ToString.Exclude
+    @OneToMany(mappedBy = "user")
+    private List<Cart> cartList;
 }
