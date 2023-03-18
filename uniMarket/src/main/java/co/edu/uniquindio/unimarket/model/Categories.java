4caddf287bcca0b3276df8c937ad70ee90660b1b
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-03-17 23:51:45 -0500

    Update Categories.java
    Se modifica la clase para que se convierta en un enum

--- uniMarket/src/main/java/co/edu/uniquindio/unimarket/model/Categories.java
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/model/Categories.java
@@ -1,24 +1,14 @@
 package co.edu.uniquindio.unimarket.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import lombok.*;
-
-import java.io.Serializable;
-
-@Entity
-@Getter
-@Setter
-@NoArgsConstructor
-@ToString
-@EqualsAndHashCode(onlyExplicitlyIncluded = true)
-public class Categories implements Serializable
+enum Categories
 {
-    @Id
-    @EqualsAndHashCode.Include
-    private int idCategory;
-
-    @Column(name = "name",nullable = false, length = 20)
-    private String name;
+    Electrodomesticos,
+    Tecnologia,
+    Hogar,
+    Moda,
+    Juegos,
+    Vehiculos,
+    Herramientas,
+    Belleza,
+    Otros,
 }
