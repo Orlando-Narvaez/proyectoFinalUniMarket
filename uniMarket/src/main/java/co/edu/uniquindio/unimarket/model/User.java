fd82131c77fc97f2e3ab160548b3de02b97e66fb
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-03-17 23:59:12 -0500

    Update User.java
    Se cambia los datos "Date" por "LocalDateTime" en la clase User

--- uniMarket/src/main/java/co/edu/uniquindio/unimarket/model/User.java
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/model/User.java
@@ -2,10 +2,9 @@ package co.edu.uniquindio.unimarket.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.ManyToOne;
 import lombok.*;
 import java.io.Serializable;
-import java.util.Date;
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -14,10 +13,9 @@ import java.util.Date;
 @ToString
 public class User extends Person implements Serializable
 {
-    @Column(name = "idCard",nullable = false)
+    @Column(name = "idCard", nullable = false)
     private int idCard;
 
     @Column(name = "creationDate",nullable = false)
-    private Date creationDate;
-
+    private LocalDateTime creationDate;
 }
