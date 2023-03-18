3181901caf1dda2a449c3dfb0b67e3f5a45a4963
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-03-17 23:52:26 -0500

    Update Comments.java
    Se cambia los datos "Date" por "LocalDateTime" en la clase Comments

--- uniMarket/src/main/java/co/edu/uniquindio/unimarket/model/Comments.java
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/model/Comments.java
@@ -4,9 +4,8 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.*;
-
 import java.io.Serializable;
-import java.util.Date;
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -14,16 +13,14 @@ import java.util.Date;
 @NoArgsConstructor
 @ToString
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
-
 public class Comments implements Serializable
 {
     @Id
     @EqualsAndHashCode.Include
-    @Column(name = "id",nullable = false)
+    @Column(name = "id", nullable = false)
     private int id;
-    @Column(name = "comment",nullable = false)
+    @Column(name = "comment", nullable = false)
     private String comment;
-    @Column(name = "dateComment",nullable = false)
-    private Date dateComment;
-
+    @Column(name = "dateComment", nullable = false)
+    private LocalDateTime dateComment;
 }
