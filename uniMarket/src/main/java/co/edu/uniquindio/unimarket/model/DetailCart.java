76f7ccede27c79413d681f6a8a0f6c72c48315bb
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-03-23 00:14:48 -0500

    Update DetailCart.java

--- uniMarket/src/main/java/co/edu/uniquindio/unimarket/model/DetailCart.java
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/model/DetailCart.java
@@ -1,10 +1,8 @@
 package co.edu.uniquindio.unimarket.model;
-
 import jakarta.persistence.*;
 import lombok.*;
 import java.io.Serializable;
 import java.util.List;
-
 @Entity
 @Getter
 @Setter
@@ -16,19 +14,14 @@ public class DetailCart implements Serializable
     @Id
     @EqualsAndHashCode.Include
     private int id;
-
     @Column(name = "value", nullable = false)
     private double value;
-
     @Column(name = "shipping", nullable = false)
     private double shipping;
-
     @Column(name = "amount", nullable = false)
     private int amount;
-
     @Column(name = "valueTotal", nullable = false)
     private double valueTotal;
-
     @ManyToOne
     private Cart cart;
 
