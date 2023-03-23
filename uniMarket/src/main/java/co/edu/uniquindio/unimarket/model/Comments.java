4e0aace0d0ea5c4926a81105ab8ca0f09dc00bc0
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-03-22 23:51:22 -0500

    Update Comments.java
    Se relaciones las entidades necesarias

--- uniMarket/src/main/java/co/edu/uniquindio/unimarket/model/Comments.java
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/model/Comments.java
@@ -1,8 +1,6 @@
 package co.edu.uniquindio.unimarket.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 import java.io.Serializable;
 import java.time.LocalDateTime;
@@ -19,8 +17,18 @@ public class Comments implements Serializable
     @EqualsAndHashCode.Include
     @Column(name = "id", nullable = false)
     private int id;
+
     @Column(name = "comment", nullable = false)
     private String comment;
+    
     @Column(name = "dateComment", nullable = false)
     private LocalDateTime dateComment;
+
+    @ToString.Exclude
+    @ManyToOne
+    private User user;
+
+    @ToString.Exclude
+    @ManyToOne
+    private Product product;
 }
