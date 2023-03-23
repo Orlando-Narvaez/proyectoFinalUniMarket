175b78776cb58cd20bdd1f5052dda280817c3458
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-03-23 01:05:09 -0500

    Update Product.java
    Se corrige error y se agrega una conexión nueva

--- uniMarket/src/main/java/co/edu/uniquindio/unimarket/model/Product.java
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/model/Product.java
@@ -1,13 +1,10 @@
 package co.edu.uniquindio.unimarket.model;
-
 import jakarta.persistence.*;
 import lombok.*;
-
 import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
-
 @Entity
 @Getter
 @Setter
@@ -19,43 +16,36 @@ public class Product implements Serializable
     @Id
     @EqualsAndHashCode.Include
     private int idProduct;
-
     @Column(name = "name", nullable = false, length = 20)
     private String name;
-
     @Column(name = "value", nullable = false)
     private double value;
-
     @Column(name = "publicationDate", nullable = false)
     private LocalDateTime publicationDate;
-
     @Column(name = "limitedDate", nullable = false)
     private LocalDateTime limitedDate;
-
     @Column(name = "description", nullable = false)
     private String description;
-
     @Column(name = "state", nullable = false)
     private Boolean state;
-
     @Column(name = "amount", nullable = false)
     private int amount;
-
     @ElementCollection
     @ToString.Exclude
     private Map<String, String> images;
-
     @ElementCollection
     @ToString.Exclude
     private List<Categories> categoriesList;
-
     @ToString.Exclude
     @ManyToMany(mappedBy = "productList")
     private List<User> userList;
-
     @ToString.Exclude
     @OneToMany(mappedBy = "product")
     private List<DetailCart> detailCartList;
+
+    @ToString.Exclude
+    @ManyToOne
+    private User user;
 
     @ToString.Exclude
     @ManyToOne
@@ -64,7 +54,6 @@ public class Product implements Serializable
     @ToString.Exclude
     @OneToMany(mappedBy = "product")
     private List<Comments> commentsList;
-
     @ToString.Exclude
     @OneToMany(mappedBy = "product")
     private List<PublicationState> publicationStateList;
