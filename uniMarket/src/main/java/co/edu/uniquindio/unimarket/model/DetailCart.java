34aa360cb4b731a9ac9a6084960e0abba00a5939
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-03-23 01:04:22 -0500

    Update DetailCart.java
    Se borra atributo innecesario

--- uniMarket/src/main/java/co/edu/uniquindio/unimarket/model/DetailCart.java
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/model/DetailCart.java
@@ -16,8 +16,7 @@ public class DetailCart implements Serializable
     private int id;
     @Column(name = "value", nullable = false)
     private double value;
-    @Column(name = "shipping", nullable = false)
-    private double shipping;
+
     @Column(name = "amount", nullable = false)
     private int amount;
     @Column(name = "valueTotal", nullable = false)
