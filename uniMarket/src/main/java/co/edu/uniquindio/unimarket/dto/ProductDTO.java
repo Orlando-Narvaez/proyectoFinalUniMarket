e9e65267068f7f22a1fc2d9bc580cd8d7d119a6b
Julian Quiroga <jaquirogab@uqvirtual.edu.co>
2023-03-24 20:32:56 -0500

    Create ProductDTO.java
    Se crea el DTO Product

---
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/dto/ProductDTO.java
@@ -0,0 +1,29 @@
+package co.edu.uniquindio.unimarket.dto;
+
+import co.edu.uniquindio.unimarket.model.Categories;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Getter
+@Setter
+
+public class ProductDTO
+{
+    private String name;
+
+    private String description;
+
+    private int units;
+
+    private float price;
+
+    private int sellerCode;
+
+    private List<String> images;
+
+    private List<Categories> categories;
+}
