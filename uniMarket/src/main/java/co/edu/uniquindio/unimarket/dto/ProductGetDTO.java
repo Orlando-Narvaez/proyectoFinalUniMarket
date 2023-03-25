71be93f9c3a2cba38eea6072ea134ee459a30153
Julian Quiroga <jaquirogab@uqvirtual.edu.co>
2023-03-24 20:33:15 -0500

    Create ProductGetDTO.java
    Se crea el DTO ProductGet

---
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/dto/ProductGetDTO.java
@@ -0,0 +1,36 @@
+package co.edu.uniquindio.unimarket.dto;
+
+import co.edu.uniquindio.unimarket.model.Categories;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@AllArgsConstructor
+@Getter
+@Setter
+
+public class ProductGetDTO
+{
+    private int code;
+
+    private boolean state;
+
+    private LocalDateTime deadLine;
+
+    private String name;
+
+    private String description;
+
+    private int units;
+
+    private float price;
+
+    private int sellerCode;
+
+    private List<String> images;
+
+    private List<Categories> categories;
+}
