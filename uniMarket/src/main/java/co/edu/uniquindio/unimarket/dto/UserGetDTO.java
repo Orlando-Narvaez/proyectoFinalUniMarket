92a61c14e253444aeebd41d5c722a4633b1ab54f
Julian Quiroga <jaquirogab@uqvirtual.edu.co>
2023-03-24 20:35:01 -0500

    Create UserGetDTO.java
    Se crea el DTO UserGet

---
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/dto/UserGetDTO.java
@@ -0,0 +1,24 @@
+package co.edu.uniquindio.unimarket.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+
+public class UserGetDTO
+{
+    private int code;
+
+    private String name;
+
+    private String email;
+
+    private String address;
+
+    private String phoneNumber;
+}
