6b24d7e625e1f0d85c7845eed50dbd79eb77cadf
Julian Quiroga <jaquirogab@uqvirtual.edu.co>
2023-03-24 20:34:08 -0500

    Create PurchaseGetDTO.java
    Se crea el DTO PurchaseGet

---
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/dto/PurchaseGetDTO.java
@@ -0,0 +1,26 @@
+package co.edu.uniquindio.unimarket.dto;
+
+import co.edu.uniquindio.unimarket.model.PaymentsMethod;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class PurchaseGetDTO
+{
+    private int code;
+
+    private LocalDateTime date;
+
+    private float totalValue;
+
+    private int userCode;
+
+    private PaymentsMethod paymentsMethod;
+
+    private List<PurchaseDetailDTO> purchaseDetailDTO;
+}
