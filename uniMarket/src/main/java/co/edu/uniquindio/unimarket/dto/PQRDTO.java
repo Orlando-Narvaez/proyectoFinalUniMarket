793cd63be0466e89d770dfb619048363d5ae313d
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-04-21 18:34:25 -0500

    Create PQRDTO.java
    Se crea la clase PQRDTO

---
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/dto/PQRDTO.java
@@ -0,0 +1,12 @@
+package co.edu.uniquindio.unimarket.dto;
+
+import java.time.LocalDateTime;
+
+public class PQRDTO
+{
+    private String description;
+
+    private Boolean state;
+
+    private LocalDateTime dateRequest;
+}
