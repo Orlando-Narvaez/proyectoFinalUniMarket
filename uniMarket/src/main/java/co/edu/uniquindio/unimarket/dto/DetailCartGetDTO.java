91b38b0494497f78d65ad9b80a660c9d57e198a8
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-04-21 13:26:29 -0500

    Create DetailCartGetDTO.java

---
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/dto/DetailCartGetDTO.java
@@ -0,0 +1,14 @@
+package co.edu.uniquindio.unimarket.dto;
+
+import co.edu.uniquindio.unimarket.model.Product;
+
+public class DetailCartGetDTO
+{
+    private int id;
+
+    private float value;
+
+    private int amount;
+
+    private int idProduct;
+}
