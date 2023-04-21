79225797597eb4a44405c37ac1c8103e526c5224
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-04-21 14:46:07 -0500

    Update PurchaseDTO.java

--- uniMarket/src/main/java/co/edu/uniquindio/unimarket/dto/PurchaseDTO.java
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/dto/PurchaseDTO.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Setter
 public class PurchaseDTO
 {
-    private int userCode;
-    private PaymentsMethod paymentsMethod;
-    private List<PurchaseDetailDTO> purchaseDetailDTO;
+    //private int userCode;
+    //private PaymentMethod paymentsMethod;
+    //private List<PurchaseDetailDTO> purchaseDetailDTO;
 }
