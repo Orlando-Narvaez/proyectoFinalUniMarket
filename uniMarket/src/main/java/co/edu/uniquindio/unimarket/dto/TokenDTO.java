f4b912288a1ab246463a838fce38e3f4e56b32d9
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-04-21 23:32:48 -0500

    Update TokenDTO.java

--- uniMarket/src/main/java/co/edu/uniquindio/unimarket/dto/TokenDTO.java
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/dto/TokenDTO.java
@@ -1,16 +1,19 @@
 package co.edu.uniquindio.unimarket.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @AllArgsConstructor
 @Getter
 @Setter
-
+@NoArgsConstructor
 public class TokenDTO
 {
+    @NotNull(message = "El token no puede ser nulo")
+    @NotBlank(message = "El token no puede estar vacío")
     private String token;
-
-    private String state;
 }
