037893a509f7a038beb6938b0305bda36d696c61
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-04-21 21:50:40 -0500

    Create PublicationStateDTO.java

---
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/dto/PublicationStateDTO.java
@@ -0,0 +1,12 @@
+package co.edu.uniquindio.unimarket.dto;
+
+import co.edu.uniquindio.unimarket.model.State;
+
+import java.util.List;
+
+public class PublicationStateDTO
+{
+    private List<State> stateList;
+
+    private String reason;
+}
