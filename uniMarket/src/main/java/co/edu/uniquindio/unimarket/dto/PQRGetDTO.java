fa35aa1a4011680cf4ef6a1dd11e7cfd6a469dd6
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-04-21 21:35:12 -0500

    Create PQRGetDTO.java

---
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/dto/PQRGetDTO.java
@@ -0,0 +1,18 @@
+package co.edu.uniquindio.unimarket.dto;
+
+import java.time.LocalDateTime;
+
+public class PQRGetDTO
+{
+    private int id;
+
+    private String description;
+
+    private Boolean state;
+
+    private LocalDateTime dateRequest;
+
+    private int idCartUser;
+
+    private int idMod;
+}
