4fad71815e8fb92c217e4c9cf07fe4ae4c9a4f8b
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-04-21 23:33:12 -0500

    Update DetailCartGetDTO.java

--- uniMarket/src/main/java/co/edu/uniquindio/unimarket/dto/DetailCartGetDTO.java
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/dto/DetailCartGetDTO.java
@@ -1,6 +1,14 @@
 package co.edu.uniquindio.unimarket.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@AllArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor
 public class DetailCartGetDTO
 {
     private int id;
