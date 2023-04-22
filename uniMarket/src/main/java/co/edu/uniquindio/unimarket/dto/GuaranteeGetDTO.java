42c3b53cb768bf1048e14b6c6e4722ce99873775
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-04-21 23:33:05 -0500

    Update GuaranteeGetDTO.java

--- uniMarket/src/main/java/co/edu/uniquindio/unimarket/dto/GuaranteeGetDTO.java
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/dto/GuaranteeGetDTO.java
@@ -1,5 +1,27 @@
 package co.edu.uniquindio.unimarket.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor
 public class GuaranteeGetDTO
 {
+    private int idGuarantee;
+
+    private String describeGuarantee;
+
+    private LocalDateTime dataRequest;
+
+    private Map<String, String> images;
+
+    private int idProduct;
+
+    private int idUser;
 }
