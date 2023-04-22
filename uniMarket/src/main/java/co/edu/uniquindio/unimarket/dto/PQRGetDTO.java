558952d9626aea43b6b07be1d5ed780093473fa7
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-04-21 23:33:00 -0500

    Update PQRGetDTO.java

--- uniMarket/src/main/java/co/edu/uniquindio/unimarket/dto/PQRGetDTO.java
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/dto/PQRGetDTO.java
@@ -1,7 +1,15 @@
 package co.edu.uniquindio.unimarket.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import java.time.LocalDateTime;
 
+@AllArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor
 public class PQRGetDTO
 {
     private int id;
