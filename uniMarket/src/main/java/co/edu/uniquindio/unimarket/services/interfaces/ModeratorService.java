88356fa450614e0c5cdc056594a28bd8c51153f5
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-04-22 00:15:42 -0500

    Create ModeratorService.java

---
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/services/interfaces/ModeratorService.java
@@ -0,0 +1,8 @@
+package co.edu.uniquindio.unimarket.services.interfaces;
+
+import co.edu.uniquindio.unimarket.model.Moderator;
+
+public interface ModeratorService
+{
+    Moderator getModeratorId(int idMod) throws Exception;
+}
