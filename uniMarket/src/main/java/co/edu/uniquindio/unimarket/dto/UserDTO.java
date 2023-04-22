2c94cff0b4b7f7cacc375802bdf7c5bf589eb005
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-04-21 23:32:45 -0500

    Update UserDTO.java

--- uniMarket/src/main/java/co/edu/uniquindio/unimarket/dto/UserDTO.java
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/dto/UserDTO.java
@@ -1,5 +1,6 @@
 package co.edu.uniquindio.unimarket.dto;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -13,28 +14,29 @@ import org.hibernate.validator.constraints.Length;
 
 public class UserDTO
 {
-    @NotNull
-    @NotBlank
+    @NotBlank(message = "El nombre no puede estar vacío")
+    @NotNull(message = "El nombre no puede ser nulo")
     @Length(max = 150, message = "El nombre debe tener máximo 100 caracteres")
     private String name;
 
-    @NotNull
-    @NotBlank
+    @NotBlank(message = "El correo no puede estar vacío")
+    @NotNull(message = "El correo no puede ser nulo")
     @Length(max = 100, message = "El correo debe tener máximo 100 caracteres")
+    @Email(message = "El correo no tiene un formato válido")
     private String email;
 
-    @NotNull
-    @NotBlank
+    @NotBlank(message = "La contraseña no puede estar vacío")
+    @NotNull(message = "La contraseña no puede ser nulo")
     @Length(max = 50, message = "La contraseña debe tener máximo 50 caracteres")
     private String password;
 
-    @NotNull
-    @NotBlank
+    @NotBlank(message = "La direccion no puede estar vacía")
+    @NotNull(message = "La direccion no puede ser nula")
     @Length(max = 100, message = "La dirección debe tener máximo 100 caracteres")
     private String address;
 
-    @NotNull
-    @NotBlank
+    @NotBlank(message = "El telefono no puede estar vacío")
+    @NotNull(message = "El telefono no puede ser nulo")
     @Length(max = 12, message = "El teléfono debe tener máximo 12 caracteres")
     private String phoneNumber;
 }
