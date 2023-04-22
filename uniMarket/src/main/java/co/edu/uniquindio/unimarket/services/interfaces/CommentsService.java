d0b5da0f6c166a1fa4bb98aa77e44f52b79c1fa3
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-04-22 00:15:34 -0500

    Update CommentsService.java

--- uniMarket/src/main/java/co/edu/uniquindio/unimarket/services/interfaces/CommentsService.java
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/services/interfaces/CommentsService.java
@@ -2,12 +2,11 @@ package co.edu.uniquindio.unimarket.services.interfaces;
 
 import co.edu.uniquindio.unimarket.dto.CommentsDTO;
 import co.edu.uniquindio.unimarket.dto.CommentsGetDTO;
-
 import java.util.List;
 
 public interface CommentsService
 {
-    int createComment(CommentsDTO commentDTO);
+    int createComment(CommentsDTO commentDTO) throws Exception;
 
-    List<CommentsGetDTO> listComments(int productCode);
+    List<CommentsGetDTO> listComments(int productCode) throws Exception;
 }
