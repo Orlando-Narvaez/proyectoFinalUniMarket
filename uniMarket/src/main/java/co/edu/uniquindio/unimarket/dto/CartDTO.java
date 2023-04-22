3d3bc8e0c7625688675536d786009e5a55a3bc8e
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-04-21 22:01:21 -0500

    Update CartDTO.java

--- uniMarket/src/main/java/co/edu/uniquindio/unimarket/dto/CartDTO.java
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/dto/CartDTO.java
@@ -1,14 +1,22 @@
 package co.edu.uniquindio.unimarket.dto;
 
 import co.edu.uniquindio.unimarket.model.PaymentMethod;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 import java.util.List;
 
 public class CartDTO
 {
-    private int idCart;
+    @NotNull(message = "El id del usuario no puede ser nulo")
+    private int idCartPerson;
 
+    @NotBlank(message = "El metodo de pago no puede estar vacío")
+    @NotNull(message = "El metodo de pago no puede ser nulo")
     private PaymentMethod PaymentMethod;
 
+    @NotNull(message = "El detalle de la compra no puede ser nulo")
+    @Size(min = 1, message = "Debe haber al menos un elemento en la lista")
     private List<DetailCartDTO> detailCartDTO;
 }
