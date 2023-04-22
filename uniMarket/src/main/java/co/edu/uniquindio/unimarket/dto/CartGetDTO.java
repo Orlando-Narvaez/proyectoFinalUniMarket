837bd15f8d9a6fe9e1822a4e8455afafe896916d
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-04-21 23:33:23 -0500

    Update CartGetDTO.java

--- uniMarket/src/main/java/co/edu/uniquindio/unimarket/dto/CartGetDTO.java
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/dto/CartGetDTO.java
@@ -2,9 +2,14 @@ package co.edu.uniquindio.unimarket.dto;
 
 import co.edu.uniquindio.unimarket.model.DetailCart;
 import co.edu.uniquindio.unimarket.model.PaymentMethod;
+import lombok.*;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@AllArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor
 public class CartGetDTO
 {
     private int idCart;
