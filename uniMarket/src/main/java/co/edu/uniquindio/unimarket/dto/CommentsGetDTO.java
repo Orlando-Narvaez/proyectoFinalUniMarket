bf150915f75754b8f3d5bf943aecacdae3c658d0
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-04-21 23:33:17 -0500

    Update CommentsGetDTO.java

--- uniMarket/src/main/java/co/edu/uniquindio/unimarket/dto/CommentsGetDTO.java
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/dto/CommentsGetDTO.java
@@ -2,13 +2,14 @@ package co.edu.uniquindio.unimarket.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
-
 import java.time.LocalDateTime;
 
 @AllArgsConstructor
 @Getter
 @Setter
+@NoArgsConstructor
 public class CommentsGetDTO
 {
     private int code;
