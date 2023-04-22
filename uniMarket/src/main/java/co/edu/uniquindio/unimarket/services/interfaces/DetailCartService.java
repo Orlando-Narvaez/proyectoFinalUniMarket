8f353cbfe1b79391c1f5a38a6f764c99a9f5841c
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-04-22 00:15:36 -0500

    Create DetailCartService.java

---
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/services/interfaces/DetailCartService.java
@@ -0,0 +1,11 @@
+package co.edu.uniquindio.unimarket.services.interfaces;
+
+import co.edu.uniquindio.unimarket.dto.DetailCartGetDTO;
+import co.edu.uniquindio.unimarket.model.DetailCart;
+
+public interface DetailCartService
+{
+    DetailCartGetDTO getCartDetail(int id) throws Exception;
+
+    DetailCart get(int id) throws Exception;
+}
