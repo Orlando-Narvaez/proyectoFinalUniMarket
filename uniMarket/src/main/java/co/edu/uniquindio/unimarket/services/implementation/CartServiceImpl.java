65858dfff519b982537710a5d7f2488dcff9096f
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-04-22 00:45:38 -0500

    Create CartServiceImpl.java

---
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/services/implementation/CartServiceImpl.java
@@ -0,0 +1,11 @@
+package co.edu.uniquindio.unimarket.services.implementation;
+
+import co.edu.uniquindio.unimarket.services.interfaces.CartService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class CartServiceImpl
+{
+}
