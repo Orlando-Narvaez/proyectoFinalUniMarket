eff73c485dd10dcd92acfc5db2892d11c7a1cf7e
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-04-21 23:33:03 -0500

    Update PQRDTO.java

--- uniMarket/src/main/java/co/edu/uniquindio/unimarket/dto/PQRDTO.java
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/dto/PQRDTO.java
@@ -1,10 +1,22 @@
 package co.edu.uniquindio.unimarket.dto;
 
-import java.time.LocalDateTime;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.*;
 
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
 public class PQRDTO
 {
+    @NotBlank(message = "La descripción no puede estar vacío")
+    @NotNull(message = "La descripción no puede ser nulo")
+    @Size(max = 500, message = "La descripción debe tener maximo 500 caracteres")
     private String description;
 
+    @NotNull(message = "El id del carrito no puede ser nulo")
     private int idCartUser;
 }
