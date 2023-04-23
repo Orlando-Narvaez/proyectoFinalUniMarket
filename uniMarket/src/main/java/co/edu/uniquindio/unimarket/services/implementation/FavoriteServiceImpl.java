baaf40f273b66c6076d575bfc22d50f35f97c306
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-04-22 20:17:27 -0500

    Create FavoriteServiceImpl.java

---
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/services/implementation/FavoriteServiceImpl.java
@@ -0,0 +1,16 @@
+package co.edu.uniquindio.unimarket.services.implementation;
+
+import co.edu.uniquindio.unimarket.dto.FavoriteDTO;
+import co.edu.uniquindio.unimarket.services.interfaces.FavoriteService;
+
+public class FavoriteServiceImpl implements FavoriteService {
+    @Override
+    public int createFavorite(FavoriteDTO favoriteDTO) throws Exception {
+        return 0;
+    }
+
+    @Override
+    public Boolean delateFavorite(int idUser, int idProduct) throws Exception {
+        return null;
+    }
+}
