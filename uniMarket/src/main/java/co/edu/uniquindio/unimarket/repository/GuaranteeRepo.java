d964e92b7815c505324b9dbd5a8fb4deb4c80334
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-04-22 20:17:12 -0500

    Create GuaranteeRepo.java

---
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/repository/GuaranteeRepo.java
@@ -0,0 +1,9 @@
+package co.edu.uniquindio.unimarket.repository;
+
+import co.edu.uniquindio.unimarket.model.Guarantee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GuaranteeRepo extends JpaRepository<Guarantee,Integer>
+{
+
+}
