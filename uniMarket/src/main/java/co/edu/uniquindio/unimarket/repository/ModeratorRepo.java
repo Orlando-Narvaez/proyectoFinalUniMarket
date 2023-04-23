03031a7be2586c897bbc6d5f284233f39fe17a4d
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-04-22 20:17:14 -0500

    Create ModeratorRepo.java

---
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/repository/ModeratorRepo.java
@@ -0,0 +1,9 @@
+package co.edu.uniquindio.unimarket.repository;
+
+import co.edu.uniquindio.unimarket.model.Moderator;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ModeratorRepo extends JpaRepository<Moderator, Integer>
+{
+
+}
