f9db4603d6d911139335bd5c483df6b6a2ce63b4
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-04-22 20:17:54 -0500

    Update FavoriteService.java

--- uniMarket/src/main/java/co/edu/uniquindio/unimarket/services/interfaces/FavoriteService.java
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/services/interfaces/FavoriteService.java
@@ -4,7 +4,7 @@ import co.edu.uniquindio.unimarket.dto.FavoriteDTO;
 
 public interface FavoriteService
 {
-    void createFavorite(FavoriteDTO favoriteDTO) throws Exception;
+    int createFavorite(FavoriteDTO favoriteDTO) throws Exception;
 
-    void eliminarFavorito(int idUser, int idProduct) throws Exception;
+    Boolean delateFavorite(int idUser, int idProduct) throws Exception;
 }
