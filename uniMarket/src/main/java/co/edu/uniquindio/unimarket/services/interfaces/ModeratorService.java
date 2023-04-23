9897b8d284d6bbe959cdbdff38d6334ad74e0b12
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-04-22 20:17:49 -0500

    Update ModeratorService.java

--- uniMarket/src/main/java/co/edu/uniquindio/unimarket/services/interfaces/ModeratorService.java
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/services/interfaces/ModeratorService.java
@@ -1,8 +1,11 @@
 package co.edu.uniquindio.unimarket.services.interfaces;
 
 import co.edu.uniquindio.unimarket.model.Moderator;
+import co.edu.uniquindio.unimarket.model.State;
 
 public interface ModeratorService
 {
     Moderator getModeratorId(int idMod) throws Exception;
+
+    int updateState(int productCode, State state) throws Exception;
 }
