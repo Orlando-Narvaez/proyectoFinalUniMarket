aa41f88da8232c6697c1bc3fd2bda01a897ba06d
Julian Quiroga <jaquirogab@uqvirtual.edu.co>
2023-04-23 18:41:55 -0500

    Update UserGetDTO.java
    se realizó actualizacion

--- uniMarket/src/main/java/co/edu/uniquindio/unimarket/dto/UserGetDTO.java
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/dto/UserGetDTO.java
@@ -1,10 +1,13 @@
 package co.edu.uniquindio.unimarket.dto;
 
+import lombok.Data;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import java.util.List;
 
+@Data
 @AllArgsConstructor
 @Getter
 @Setter
