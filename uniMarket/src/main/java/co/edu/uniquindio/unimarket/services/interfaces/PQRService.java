ccc1528099035ebfcb85c750dc3fe9c7ed1bf84d
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-04-22 20:17:47 -0500

    Update PQRService.java

--- uniMarket/src/main/java/co/edu/uniquindio/unimarket/services/interfaces/PQRService.java
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/services/interfaces/PQRService.java
@@ -1,4 +1,15 @@
 package co.edu.uniquindio.unimarket.services.interfaces;
 
-public interface PQRService {
+import co.edu.uniquindio.unimarket.dto.PQRDTO;
+import co.edu.uniquindio.unimarket.dto.PQRGetDTO;
+
+import java.util.List;
+
+public interface PQRService
+{
+    int createPQR(PQRDTO pqrDTO) throws Exception;
+
+    List<PQRDTO> listPQR(int id) throws Exception;
+
+    PQRGetDTO getPQR(int id) throws Exception;
 }
