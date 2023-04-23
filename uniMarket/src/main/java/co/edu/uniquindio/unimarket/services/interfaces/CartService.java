8b22163e9f6199be04eaafba4b861eaa1b3752aa
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-04-22 20:18:01 -0500

    Update CartService.java

--- uniMarket/src/main/java/co/edu/uniquindio/unimarket/services/interfaces/CartService.java
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/services/interfaces/CartService.java
@@ -2,13 +2,19 @@ package co.edu.uniquindio.unimarket.services.interfaces;
 
 import co.edu.uniquindio.unimarket.dto.CartDTO;
 import co.edu.uniquindio.unimarket.dto.CartGetDTO;
+import co.edu.uniquindio.unimarket.dto.UserDTO;
+
 import java.util.List;
 
 public interface CartService
 {
     int createCart(CartDTO cartDTO) throws Exception;
 
-    List<CartDTO> listCart(int userCode) throws Exception;
+    int updateCart(int idCart, CartDTO cartDTO) throws Exception;
+
+    int deleteCart(int idCart) throws Exception;
+
+    List<CartDTO> listCart(int idCart) throws Exception;
 
     CartGetDTO getCart(int idCart) throws Exception;
 }
