afba171139a21af9e0c4aff486903d218b7ce85f
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-04-22 20:17:29 -0500

    Create GuaranteeServiceImpl.java

---
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/services/implementation/GuaranteeServiceImpl.java
@@ -0,0 +1,24 @@
+package co.edu.uniquindio.unimarket.services.implementation;
+
+import co.edu.uniquindio.unimarket.dto.GuaranteeDTO;
+import co.edu.uniquindio.unimarket.dto.GuaranteeGetDTO;
+import co.edu.uniquindio.unimarket.services.interfaces.GuaranteeService;
+
+import java.util.List;
+
+public class GuaranteeServiceImpl implements GuaranteeService {
+    @Override
+    public int createGuarantee(GuaranteeDTO guaranteeDTO) throws Exception {
+        return 0;
+    }
+
+    @Override
+    public List<GuaranteeDTO> listGuarantee(int idGuarantee) throws Exception {
+        return null;
+    }
+
+    @Override
+    public GuaranteeGetDTO getGuarantee(int idGuarantee) throws Exception {
+        return null;
+    }
+}
