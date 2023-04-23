444df86b3eefdf407e4fbf7d6cec93e95e270a18
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-04-22 20:17:45 -0500

    Update ProductService.java

--- uniMarket/src/main/java/co/edu/uniquindio/unimarket/services/interfaces/ProductService.java
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/services/interfaces/ProductService.java
@@ -3,6 +3,7 @@ package co.edu.uniquindio.unimarket.services.interfaces;
 import co.edu.uniquindio.unimarket.dto.ProductDTO;
 import co.edu.uniquindio.unimarket.dto.ProductGetDTO;
 import co.edu.uniquindio.unimarket.model.Categories;
+import co.edu.uniquindio.unimarket.model.Product;
 import co.edu.uniquindio.unimarket.model.State;
 
 import java.util.List;
@@ -10,25 +11,27 @@ public interface ProductService
 {
     int createProduct(ProductDTO productDTO) throws Exception;
 
-    int updateProduct(int productCode, ProductDTO productDTO) throws Exception;
+    int updateProduct(int idProduct, ProductDTO productDTO) throws Exception;
 
-    int updateUnits(int productCode, int units) throws Exception;
+    int updateUnits(int idProduct, int amount) throws Exception;
 
-    int updateState(int productCode, State state) throws Exception;
+    int updateState(int idProduct, boolean state) throws Exception;
 
-    int deleteProduct(int productCode) throws Exception;
+    int deleteProduct(int idProduct) throws Exception;
 
-    ProductGetDTO getProduct(int productCode) throws Exception;
+    ProductGetDTO getProduct(int idProduct) throws Exception;
 
-    List<ProductGetDTO> listUserProducts(int productCode) throws Exception;
+    List<ProductGetDTO> listUserProducts(int idProduct) throws Exception;
 
     List<ProductGetDTO> listCategoriesProducts(Categories categories) throws Exception;
 
-    List<ProductGetDTO> listProductsByState(State state) throws Exception;
+    List<ProductGetDTO> listProductsByState(boolean state) throws Exception;
 
-    List<ProductGetDTO> listFavoriteProducts(int userCode) throws Exception;
+    List<ProductGetDTO> listFavoriteProducts(int idUser) throws Exception;
 
     List<ProductGetDTO> listProductsName(String name) throws Exception;
 
     List<ProductGetDTO> listProductPrice(float minimalPrice, float maximumPrice) throws Exception;
+
+    Product search(int idProduct) throws Exception;
 }
