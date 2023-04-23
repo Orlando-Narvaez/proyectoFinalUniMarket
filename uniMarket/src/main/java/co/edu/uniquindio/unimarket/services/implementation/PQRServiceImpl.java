84d40a8f0c6022593e2532929f355ada7b8ba293
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-04-22 20:17:38 -0500

    Create PQRServiceImpl.java

---
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/services/implementation/PQRServiceImpl.java
@@ -0,0 +1,24 @@
+package co.edu.uniquindio.unimarket.services.implementation;
+
+import co.edu.uniquindio.unimarket.dto.PQRDTO;
+import co.edu.uniquindio.unimarket.dto.PQRGetDTO;
+import co.edu.uniquindio.unimarket.services.interfaces.PQRService;
+
+import java.util.List;
+
+public class PQRServiceImpl implements PQRService {
+    @Override
+    public int createPQR(PQRDTO pqrDTO) throws Exception {
+        return 0;
+    }
+
+    @Override
+    public List<PQRDTO> listPQR(int id) throws Exception {
+        return null;
+    }
+
+    @Override
+    public PQRGetDTO getPQR(int id) throws Exception {
+        return null;
+    }
+}
