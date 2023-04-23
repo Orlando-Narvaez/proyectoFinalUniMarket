2c0c9c0881e0ffa0e1fbfcbc94ce712d87b2cf7a
Julian Quiroga <jaquirogab@uqvirtual.edu.co>
2023-04-22 20:50:48 -0500

    Update ModeratorRepo.java
    Se realiza modificacion

--- uniMarket/src/main/java/co/edu/uniquindio/unimarket/repository/ModeratorRepo.java
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/repository/ModeratorRepo.java
@@ -1,9 +1,12 @@
 package co.edu.uniquindio.unimarket.repository;
 
 import co.edu.uniquindio.unimarket.model.Moderator;
+import co.edu.uniquindio.unimarket.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ModeratorRepo extends JpaRepository<Moderator, Integer>
 {
-
+    @Query("select m from Moderator m where m.email = :email")
+    Moderator findByEmail(String email);
 }
