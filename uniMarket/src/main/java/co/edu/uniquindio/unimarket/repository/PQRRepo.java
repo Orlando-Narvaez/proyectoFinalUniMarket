e708dc2059e75f5d850909520742a069f4a5d2bc
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-04-22 20:17:17 -0500

    Create PQRRepo.java

---
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/repository/PQRRepo.java
@@ -0,0 +1,9 @@
+package co.edu.uniquindio.unimarket.repository;
+
+import co.edu.uniquindio.unimarket.model.PQR;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PQRRepo extends JpaRepository<PQR, Integer>
+{
+
+}
