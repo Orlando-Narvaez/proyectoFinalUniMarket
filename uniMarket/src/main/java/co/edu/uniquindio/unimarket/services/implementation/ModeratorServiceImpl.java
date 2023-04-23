80a269037f83c2db4e85ea3fdc50572c6616de05
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-04-22 20:17:36 -0500

    Create ModeratorServiceImpl.java

---
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/services/implementation/ModeratorServiceImpl.java
@@ -0,0 +1,18 @@
+package co.edu.uniquindio.unimarket.services.implementation;
+
+import co.edu.uniquindio.unimarket.model.Moderator;
+import co.edu.uniquindio.unimarket.model.State;
+import co.edu.uniquindio.unimarket.services.interfaces.ModeratorService;
+
+public class ModeratorServiceImpl implements ModeratorService
+{
+    @Override
+    public Moderator getModeratorId(int idMod) throws Exception {
+        return null;
+    }
+
+    @Override
+    public int updateState(int productCode, State state) throws Exception {
+        return 0;
+    }
+}
