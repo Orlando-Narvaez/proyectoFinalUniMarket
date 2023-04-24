68b56fa5a79629951a003067a50fb4d2d14258fe
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-04-23 23:31:55 -0500

    Update UserRepo.java

--- uniMarket/src/main/java/co/edu/uniquindio/unimarket/repository/UserRepo.java
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/repository/UserRepo.java
@@ -3,7 +3,9 @@ package co.edu.uniquindio.unimarket.repository;
 import co.edu.uniquindio.unimarket.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepo extends JpaRepository<User, Integer>
 {
     @Query("select u from User u where u.email = :email")
