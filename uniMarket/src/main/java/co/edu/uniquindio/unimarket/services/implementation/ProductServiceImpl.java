ac763e37376655869c8a928da5dbb065b563f730
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-04-24 02:34:20 -0500

    Update ProductServiceImpl.java

--- uniMarket/src/main/java/co/edu/uniquindio/unimarket/services/implementation/ProductServiceImpl.java
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/services/implementation/ProductServiceImpl.java
@@ -4,83 +4,235 @@ import co.edu.uniquindio.unimarket.dto.ProductDTO;
 import co.edu.uniquindio.unimarket.dto.ProductGetDTO;
 import co.edu.uniquindio.unimarket.model.Categories;
 import co.edu.uniquindio.unimarket.model.Product;
+import co.edu.uniquindio.unimarket.model.State;
+import co.edu.uniquindio.unimarket.model.User;
 import co.edu.uniquindio.unimarket.repository.ProductRepo;
 import co.edu.uniquindio.unimarket.services.interfaces.ProductService;
+import co.edu.uniquindio.unimarket.services.interfaces.UserService;
 import lombok.AllArgsConstructor;
-
+import org.springframework.stereotype.Service;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+@Service
 @AllArgsConstructor
 public class ProductServiceImpl implements ProductService
 {
-    private  final ProductRepo productRepo;
+    private final ProductRepo productRepo;
+    private final UserService userService;
+
     @Override
-    public int createProduct(ProductDTO productDTO) throws Exception {
-        Product product = new Product();
-        return productRepo.save(product).getIdProduct();
+    public int createProduct(ProductDTO productDTO) throws Exception
+    {
+        Product newProduct = new Product();
+
+        newProduct.setName(productDTO.getName());
+        newProduct.setValue(productDTO.getValue());
+        newProduct.setPublicationDate(LocalDateTime.now());
+        newProduct.setLimitedDate(LocalDateTime.now().plusDays(30));
+        newProduct.setDescription(productDTO.getDescription());
+        newProduct.setState(State.INACTIVO);
+        newProduct.setSeller(userService.getUserDataBase(productDTO.getSellerCode()));
+        newProduct.setImages(productDTO.getImagenes());
+        newProduct.setCategoriesList(productDTO.getCategories());
+
+        return productRepo.save(newProduct).getIdProduct();
     }
 
     @Override
-    public int updateProduct(int idProduct, ProductDTO productDTO) throws Exception {
+    public int delateProduct(int idProduct) throws Exception
+    {
+        Product product = null;
+
+        if (confirmProduct(idProduct))
+        {
+            product = getProductBD(idProduct);
+            product.setState(State.INACTIVO);
+        }
+
         return 0;
     }
 
     @Override
-    public int updateUnits(int idProduct, int amount) throws Exception {
+    public int updateProduct(int idProduct, ProductDTO productDTO) throws Exception
+    {
+        Product product = null;
+
+        if (confirmProduct(idProduct))
+        {
+            product = getProductBD(idProduct);
+
+            product.setIdProduct(idProduct);
+            product.setName(productDTO.getName());
+            product.setValue(productDTO.getValue());
+            product.setDescription(productDTO.getDescription());
+            product.setImages(productDTO.getImagenes());
+            product.setCategoriesList(productDTO.getCategories());
+
+            return productRepo.save(product).getIdProduct();
+        }
+
         return 0;
     }
 
     @Override
-    public int updateState(int idProduct, boolean state) throws Exception {
+    public int updateForState(int idProduct, State state) throws Exception
+    {
+        Product product = null;
+
+        if (confirmProduct(idProduct))
+        {
+            product = getProductBD(idProduct);
+            product.setState(state);
+
+            return productRepo.save(product).getIdProduct();
+        }
+
         return 0;
     }
 
     @Override
-    public int deleteProduct(int idProduct) throws Exception {
-        Product product = new Product();
-        return  0; //productRepo.deleteById(idProduct);
+    public ProductGetDTO getProduct(int idProduct) throws Exception
+    {
+        ProductGetDTO productGetDTO = convertProduct(getProductBD(idProduct));
+
+        return productGetDTO;
     }
 
     @Override
-    public ProductGetDTO getProduct(int idProduct) throws Exception {
-
+    public Product getProductBD(int idProduct) throws Exception
+    {
         Optional<Product> product = productRepo.findById(idProduct);
-        return null;
+
+        if (product.isEmpty())
+        {
+            throw new Exception("El producto con el código " + idProduct + " no existe en la base de datos");
+        }
+
+        return product.get();
     }
 
     @Override
-    public List<ProductGetDTO> listUserProducts(int idProduct) throws Exception {
-        return null;
+    public List<ProductGetDTO> listProductForCategorie(Categories categories) throws Exception
+    {
+        List<ProductGetDTO> lstAnswer = new ArrayList<>();
+        List<Product> lstProduct = productRepo.getProductCategories(categories);
+
+        if (lstProduct != null && lstProduct.size() > 0)
+        {
+            lstProduct.forEach(product -> {
+                lstAnswer.add(convertProduct(product));
+            });
+        }
+
+        return lstAnswer;
     }
 
     @Override
-    public List<ProductGetDTO> listCategoriesProducts(Categories categories) throws Exception {
-        return null;
+    public List<ProductGetDTO> listProductUser(int idCard) throws Exception
+    {
+        List<ProductGetDTO> lstAnswerProductUser = new ArrayList<>();
+        List<Product> lstProduct = productRepo.getProductUsers(idCard);
+
+        if (lstProduct != null && lstProduct.size() > 0)
+        {
+            lstProduct.forEach(product -> {
+                lstAnswerProductUser.add(convertProduct(product));
+            });
+        }
+
+        return lstAnswerProductUser;
     }
 
     @Override
-    public List<ProductGetDTO> listProductsByState(boolean state) throws Exception {
-        return null;
+    public List<ProductGetDTO> listProductFavUser(int idCard) throws Exception
+    {
+        List<ProductGetDTO> lstAnswerProduct = new ArrayList<>();
+        User user = userService.getUserDataBase(idCard);
+        List<Product> lstProducts = productRepo.getProductsFavs(user);
+
+        if (lstProducts != null && lstProducts.size() > 0 )
+        {
+            lstProducts.forEach(product -> {
+                lstAnswerProduct.add(convertProduct(product));
+            });
+        }
+
+        return lstAnswerProduct;
     }
 
     @Override
-    public List<ProductGetDTO> listFavoriteProducts(int idUser) throws Exception {
-        return null;
+    public List<ProductGetDTO> listProductPrices(double priceMin, double priceMax) throws Exception
+    {
+        List<ProductGetDTO> lstAnswerProductForPrices = new ArrayList<>();
+        List<Product> lstProducts = productRepo.getProductRangePrice(priceMin,priceMax);
+
+        if (lstProducts != null && lstProducts.size() > 0)
+        {
+            lstProducts.forEach(product -> {
+                lstAnswerProductForPrices.add(convertProduct(product));
+            });
+        }
+
+        return lstAnswerProductForPrices;
     }
 
     @Override
-    public List<ProductGetDTO> listProductsName(String name) throws Exception {
-        return null;
+    public List<ProductGetDTO> listProductForNome(String name)
+    {
+        List<ProductGetDTO> lstAnswerProductForNames = new ArrayList<>();
+        List<Product> lstProducts = productRepo.getProductName(name);
+
+        if (lstProducts != null && lstProducts.size() > 0)
+        {
+            lstProducts.forEach(product -> {
+                lstAnswerProductForNames.add(convertProduct(product));
+            });
+        }
+
+        return lstAnswerProductForNames;
     }
 
     @Override
-    public List<ProductGetDTO> listProductPrice(float minimalPrice, float maximumPrice) throws Exception {
-        List<Product> lstPRoducts = productRepo.getProductsWithRange(minimalPrice, maximumPrice);
+    public List<ProductGetDTO> convertLstProduct(List<Product> products)
+    {
+        List<ProductGetDTO> lstAnswerProducts = new ArrayList<>();
+
+        products.forEach(product -> {
+            lstAnswerProducts.add(convertProduct(product));
+        });
+
+        return lstAnswerProducts;
     }
 
-    @Override
-    public Product search(int idProduct) throws Exception {
-        return null;
+    private ProductGetDTO convertProduct(Product product)
+    {
+        ProductGetDTO productGetDTO = new ProductGetDTO(
+                product.getIdProduct(),
+                product.getName(),
+                product.getValue(),
+                product.getPublicationDate(),
+                product.getLimitedDate(),
+                product.getDescription(),
+                product.getState(),
+                product.getSeller().getIdCard(),
+                product.getImages(),
+                product.getCategoriesList()
+        );
+
+        return productGetDTO;
+    }
+
+    private boolean confirmProduct(int idProduct) throws Exception {
+        boolean search = productRepo.existsById(idProduct);
+
+        if (!search)
+        {
+            throw new Exception("El código " + idProduct + " no esta asociado a ningún producto");
+        }
+
+        return search;
     }
 }
