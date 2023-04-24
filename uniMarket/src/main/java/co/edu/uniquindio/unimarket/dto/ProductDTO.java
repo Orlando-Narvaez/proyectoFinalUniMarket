d687c30fdf0fb60e8fb2d6f2927f31e9e8c64cc1
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-04-24 02:33:53 -0500

    Update ProductDTO.java

--- uniMarket/src/main/java/co/edu/uniquindio/unimarket/dto/ProductDTO.java
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/dto/ProductDTO.java
@@ -18,20 +18,14 @@ public class ProductDTO
     @Size(max = 100, message = "El nombre del producto no puede tener más de 100 caracteres")
     private String name;
 
+    @NotNull(message = "El precio del producto no puede ser nulo")
+    @PositiveOrZero(message = "El precio del producto debe ser mayor o igual a cero")
+    private double value;
+
     @NotNull(message = "La descripción del producto no puede ser nula")
     @NotBlank(message = "La descripción del producto no puede estar vacía")
     @Size(max = 500, message = "La descripción del producto no puede tener más de 500 caracteres")
     private String description;
-
-    @NotNull(message = "El precio del producto no puede ser nulo")
-    @Min(value = 1, message = "El número de unidades disponibles debe ser mayor o igual a 1")
-    @Max(value = 100, message = "El número de unidades disponibles no puede ser mayor a 100")
-    @PositiveOrZero(message = "El número de unidades disponibles debe ser mayor o igual a cero")
-    private int units;
-
-    @NotNull(message = "El precio del producto no puede ser nulo")
-    @PositiveOrZero(message = "El precio del producto debe ser mayor o igual a cero")
-    private float value;
 
     @NotNull(message = "El id del vendedor no puede ser nulo")
     private int sellerCode;
