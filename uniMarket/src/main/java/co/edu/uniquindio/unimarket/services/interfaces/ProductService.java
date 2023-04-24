be55906d8f2eef3842ecdc8d14b0c62f2b107eb4
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-04-24 02:34:26 -0500

    Update ProductService.java

--- uniMarket/src/main/java/co/edu/uniquindio/unimarket/services/interfaces/ProductService.java
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/services/interfaces/ProductService.java
@@ -11,29 +11,24 @@ public interface ProductService
 {
     int createProduct(ProductDTO productDTO) throws Exception;
 
+    int delateProduct(int idProduct) throws Exception;
+
     int updateProduct(int idProduct, ProductDTO productDTO) throws Exception;
 
-    int updateUnits(int idProduct, int amount) throws Exception;
-
-    int updateState(int idProduct, boolean state) throws Exception;
-
-    int deleteProduct(int idProduct) throws Exception;
+    int updateForState(int idProduct, State state) throws Exception;
 
     ProductGetDTO getProduct(int idProduct) throws Exception;
 
-    Product getProductDataBase(int idProduct)throws Exception;
+    Product getProductBD(int idProduct) throws Exception;
 
-    List<ProductGetDTO> listUserProducts(int idProduct) throws Exception;
+    List<ProductGetDTO> listProductForCategorie(Categories categories) throws Exception;
 
-    List<ProductGetDTO> listCategoriesProducts(Categories categories) throws Exception;
+    List<ProductGetDTO> listProductUser(int idCard) throws Exception;
 
-    List<ProductGetDTO> listProductsByState(boolean state) throws Exception;
+    List<ProductGetDTO> listProductFavUser(int idCard) throws Exception;
 
-    List<ProductGetDTO> listFavoriteProducts(int idUser) throws Exception;
+    List<ProductGetDTO> listProductPrices(double priceMin, double priceMax) throws Exception;
 
-    List<ProductGetDTO> listProductsName(String name) throws Exception;
-
-    List<ProductGetDTO> listProductPrice(float minimalPrice, float maximumPrice) throws Exception;
-
-    Product search(int idProduct) throws Exception;
+    List<ProductGetDTO> listProductForNome(String name);
+    List<ProductGetDTO> convertLstProduct(List<Product> products);
 }
