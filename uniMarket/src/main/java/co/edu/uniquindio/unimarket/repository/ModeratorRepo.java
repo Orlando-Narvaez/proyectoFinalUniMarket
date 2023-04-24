075a7bb7d89e50f4ac4eb8d120f1d7c5080540e9
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-04-24 02:34:11 -0500

    Update ModeratorRepo.java

--- uniMarket/src/main/java/co/edu/uniquindio/unimarket/repository/ModeratorRepo.java
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/repository/ModeratorRepo.java
@@ -1,12 +1,10 @@
 package co.edu.uniquindio.unimarket.repository;
 
 import co.edu.uniquindio.unimarket.model.Moderator;
-import co.edu.uniquindio.unimarket.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ModeratorRepo extends JpaRepository<Moderator, Integer>
 {
-    @Query("select m from Moderator m where m.email = :email")
-    Moderator findByEmail(String email);
 }
