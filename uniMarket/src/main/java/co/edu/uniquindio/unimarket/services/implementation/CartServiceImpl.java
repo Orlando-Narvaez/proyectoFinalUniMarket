8cf5731b6a79466a51f8d8a3f96d1e24ff6f73d4
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-04-24 23:25:21 -0500

    Update CartServiceImpl.java

--- uniMarket/src/main/java/co/edu/uniquindio/unimarket/services/implementation/CartServiceImpl.java
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/services/implementation/CartServiceImpl.java
@@ -3,45 +3,46 @@ package co.edu.uniquindio.unimarket.services.implementation;
 import co.edu.uniquindio.unimarket.dto.CartDTO;
 import co.edu.uniquindio.unimarket.dto.CartGetDTO;
 import co.edu.uniquindio.unimarket.model.Cart;
+import co.edu.uniquindio.unimarket.model.StateCart;
 import co.edu.uniquindio.unimarket.repository.CartRepo;
 import co.edu.uniquindio.unimarket.services.interfaces.CartService;
+import co.edu.uniquindio.unimarket.services.interfaces.UserService;
 import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 @AllArgsConstructor
 public class CartServiceImpl implements CartService
 {
-    private  final CartRepo cartRepo;
+    private final CartRepo cartRepo;
+    private final 
+    private final UserService userService;
     @Override
     public int createCart(CartDTO cartDTO) throws Exception
     {
-        Cart cart = new Cart();
-        return cartRepo.save(cart).getIdCart();
-    }
-
-    @Override
-    public int updateCart(int idCart, CartDTO cartDTO) throws Exception {
-        Cart cart = new Cart();
         return 0;
     }
 
     @Override
-    public int deleteCart(int idCart) throws Exception {
-        return 0;
-    }
-
-    @Override
-    public List<CartDTO> listCart(int idCart) throws Exception {
-        Optional<Cart> cart = cartRepo.findById(idCart);
+    public CartGetDTO getCart(int idCart) throws Exception {
         return null;
     }
 
     @Override
-    public CartGetDTO getCart(int idCart) throws Exception
-    {
+    public Cart getCartBD(int idCart) throws Exception {
+        return null;
+    }
 
+    @Override
+    public List<CartGetDTO> listCartForUser(int idCard) throws Exception {
+        return null;
+    }
+
+    @Override
+    public List<CartGetDTO> listCartForState(StateCart stateCart) throws Exception {
         return null;
     }
 }
