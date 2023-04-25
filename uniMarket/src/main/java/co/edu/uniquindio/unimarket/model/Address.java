ed7580a685a7d1a65830f5b3aec39decb48749f2
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-04-24 23:25:12 -0500

    Create Address.java

---
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/model/Address.java
@@ -0,0 +1,33 @@
+package co.edu.uniquindio.unimarket.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.io.Serializable;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Address implements Serializable
+{
+    @Id
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(nullable = false)
+    private String descripcion;
+    @Column(nullable = false)
+    private int postalCod;
+    @ManyToOne
+    @ToString.Exclude
+    private User user;
+    @ManyToOne
+    @ToString.Exclude
+    private Moderator moderator;
+    @OneToMany(mappedBy = "Address")
+    @ToString.Exclude
+    private List<Cart> cartList;
+}
