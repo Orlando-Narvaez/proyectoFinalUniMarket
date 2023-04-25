42d944b6841cd83778c69e7e1c71d9bf2e5f8723
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-04-24 23:25:24 -0500

    Update DetailCartImpl.java

--- uniMarket/src/main/java/co/edu/uniquindio/unimarket/services/implementation/DetailCartImpl.java
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/services/implementation/DetailCartImpl.java
@@ -1,14 +1,37 @@
 package co.edu.uniquindio.unimarket.services.implementation;
 
+import co.edu.uniquindio.unimarket.dto.DetailCartDTO;
 import co.edu.uniquindio.unimarket.dto.DetailCartGetDTO;
+import co.edu.uniquindio.unimarket.model.DetailCart;
 import co.edu.uniquindio.unimarket.repository.DetailCartRepo;
+import co.edu.uniquindio.unimarket.services.interfaces.CartService;
 import co.edu.uniquindio.unimarket.services.interfaces.DetailCartService;
+import co.edu.uniquindio.unimarket.services.interfaces.ProductService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
+@AllArgsConstructor
 public class DetailCartImpl implements DetailCartService
 {
+    private final ProductService productService;
+    private final CartService cartService;
+    private final DetailCartRepo detailCartRepo;
 
     @Override
-    public DetailCartGetDTO getCartDetail(int id) throws Exception {
+    public int createDetailCart(int id, DetailCartDTO detailCartDTO) throws Exception {
+        return 0;
+    }
+
+    @Override
+    public List<DetailCartGetDTO> getDetailsForCart(int id) throws Exception {
+        return null;
+    }
+
+    @Override
+    public List<DetailCartGetDTO> convertLista(List<DetailCart> lstDetailCart) {
         return null;
     }
 }
