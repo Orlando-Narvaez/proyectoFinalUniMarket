4b546f04b74e01e0b51eb4203c64430e93ba6e1c
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-04-24 23:25:26 -0500

    Update FavoriteServiceImpl.java

--- uniMarket/src/main/java/co/edu/uniquindio/unimarket/services/implementation/FavoriteServiceImpl.java
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/services/implementation/FavoriteServiceImpl.java
@@ -1,16 +1,48 @@
 package co.edu.uniquindio.unimarket.services.implementation;
 
 import co.edu.uniquindio.unimarket.dto.FavoriteDTO;
+import co.edu.uniquindio.unimarket.model.Product;
+import co.edu.uniquindio.unimarket.model.User;
 import co.edu.uniquindio.unimarket.services.interfaces.FavoriteService;
+import co.edu.uniquindio.unimarket.services.interfaces.ProductService;
+import co.edu.uniquindio.unimarket.services.interfaces.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
 
-public class FavoriteServiceImpl implements FavoriteService {
+@Service
+@AllArgsConstructor
+public class FavoriteServiceImpl implements FavoriteService
+{
+    private final ProductService productService;
+    private final UserService userService;
+
     @Override
-    public int createFavorite(FavoriteDTO favoriteDTO) throws Exception {
-        return 0;
+    public boolean createFavorite(FavoriteDTO favoriteDTO) throws Exception
+    {
+        User user = userService.getUserDataBase(favoriteDTO.getIdUser());
+        Product product = productService.getProductBD(favoriteDTO.getIdProduct());
+
+        if (user.getFavorite().contains(product))
+        {
+            user.getFavorite().add(product);
+            return userService.createProductFavUser(user);
+        }
+
+        return false;
     }
 
     @Override
-    public Boolean delateFavorite(int idUser, int idProduct) throws Exception {
-        return null;
+    public boolean delateFavorite(FavoriteDTO favoriteDTO) throws Exception
+    {
+        User user = userService.getUserDataBase(favoriteDTO.getIdUser());
+        Product product = productService.getProductBD(favoriteDTO.getIdProduct());
+
+        if (user.getFavorite().contains(product))
+        {
+            user.getFavorite().remove(product);
+            return userService.createProductFavUser(user);
+        }
+
+        throw new Exception("EL producto con él código "+favoriteDTO+ " no se encuentra en la lista de favoritos");
     }
 }
