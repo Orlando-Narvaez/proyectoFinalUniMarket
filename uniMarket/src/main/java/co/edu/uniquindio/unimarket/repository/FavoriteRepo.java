d00c3499abf4ddf7b0efd1615daf9cf783a28849
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-04-25 13:08:31 -0500

    Update FavoriteRepo.java

--- uniMarket/src/main/java/co/edu/uniquindio/unimarket/repository/FavoriteRepo.java
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/repository/FavoriteRepo.java
@@ -3,6 +3,10 @@ package co.edu.uniquindio.unimarket.repository;
 import co.edu.uniquindio.unimarket.model.DetailCart;
 import co.edu.uniquindio.unimarket.model.Favorite;
 import org.hibernate.metamodel.model.convert.spi.JpaAttributeConverter;
+import org.springframework.stereotype.Repository;
 
-public interface FavoriteRepo extends JpaAttributeConverter<Favorite, Integer> {
+@Repository
+public interface FavoriteRepo extends JpaAttributeConverter<Favorite, Integer>
+{
+
 }
