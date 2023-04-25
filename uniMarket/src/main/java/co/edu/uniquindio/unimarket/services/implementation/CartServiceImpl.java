9055353963830a4bd7d1d54a75e76df78df37b96
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-04-25 01:37:59 -0500

    Update CartServiceImpl.java

--- uniMarket/src/main/java/co/edu/uniquindio/unimarket/services/implementation/CartServiceImpl.java
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/services/implementation/CartServiceImpl.java
@@ -2,14 +2,19 @@ package co.edu.uniquindio.unimarket.services.implementation;
 
 import co.edu.uniquindio.unimarket.dto.CartDTO;
 import co.edu.uniquindio.unimarket.dto.CartGetDTO;
+import co.edu.uniquindio.unimarket.dto.DetailCartGetDTO;
 import co.edu.uniquindio.unimarket.model.Cart;
+import co.edu.uniquindio.unimarket.model.Product;
 import co.edu.uniquindio.unimarket.model.StateCart;
+import co.edu.uniquindio.unimarket.model.User;
 import co.edu.uniquindio.unimarket.repository.CartRepo;
-import co.edu.uniquindio.unimarket.services.interfaces.CartService;
-import co.edu.uniquindio.unimarket.services.interfaces.UserService;
+import co.edu.uniquindio.unimarket.services.interfaces.*;
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,8 +23,11 @@ import java.util.Optional;
 public class CartServiceImpl implements CartService
 {
     private final CartRepo cartRepo;
-    private final 
+    private final DetailCartService detailCartService;
     private final UserService userService;
+    private final ProductService productService;
+    private final EmailService emailService;
+    private final AddressService addressService;
     @Override
     public int createCart(CartDTO cartDTO) throws Exception
     {
@@ -27,22 +35,45 @@ public class CartServiceImpl implements CartService
     }
 
     @Override
-    public CartGetDTO getCart(int idCart) throws Exception {
-        return null;
+    public CartGetDTO getCart(int idCart) throws Exception
+    {
+        return convertCart(getCartBD(idCart));
     }
 
     @Override
-    public Cart getCartBD(int idCart) throws Exception {
-        return null;
+    public Cart getCartBD(int idCart) throws Exception
+    {
+        Optional<Cart> compra = cartRepo.findById(idCart);
+        if (compra.isEmpty()) {
+            throw new Exception("La compra asociada al código " + idCart + " no existe");
+        }
+
+        return compra.get();
     }
 
     @Override
     public List<CartGetDTO> listCartForUser(int idCard) throws Exception {
-        return null;
+        List<CartGetDTO> lstCartAnswer= new ArrayList<>();
+        List<Cart> lstCart = cartRepo.getLstCartForUser(idCard);
+        if (lstCart != null && lstCart.size() > 0) {
+            lstCart.forEach(compra -> {
+                lstCartAnswer.add(convertCart(compra));
+            });
+        }
+
+        return lstCartAnswer;
     }
 
-    @Override
-    public List<CartGetDTO> listCartForState(StateCart stateCart) throws Exception {
-        return null;
+    private CartGetDTO convertCart(Cart cart)
+    {
+        CartGetDTO cartGetDTO = new CartGetDTO(
+                cart.getIdCart(),
+                cart.getBuyDate(),
+                cart.getUser().getIdUser(),
+                cart.getTotalValue(),
+                detailCartService.convertLista(cart.getDetailCartList()),
+        );
+
+        return cartGetDTO;
     }
 }
