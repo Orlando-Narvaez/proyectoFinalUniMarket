f156b19728c5d0ddeb91a68e0528ffd86c0e4af2
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-04-25 13:08:36 -0500

    Update PQRServiceImpl.java

--- uniMarket/src/main/java/co/edu/uniquindio/unimarket/services/implementation/PQRServiceImpl.java
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/services/implementation/PQRServiceImpl.java
@@ -2,23 +2,69 @@ package co.edu.uniquindio.unimarket.services.implementation;
 
 import co.edu.uniquindio.unimarket.dto.PQRDTO;
 import co.edu.uniquindio.unimarket.dto.PQRGetDTO;
+import co.edu.uniquindio.unimarket.model.PQR;
+import co.edu.uniquindio.unimarket.repository.PQRRepo;
 import co.edu.uniquindio.unimarket.services.interfaces.PQRService;
-
+import co.edu.uniquindio.unimarket.services.interfaces.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
-public class PQRServiceImpl implements PQRService {
+@Service
+@AllArgsConstructor
+public class PQRServiceImpl implements PQRService
+{
+    private final PQRRepo pqrRepo;
+    private final UserService userService;
+
     @Override
-    public int createPQR(PQRDTO pqrDTO) throws Exception {
-        return 0;
+    public int createPQR(PQRDTO pqrDTO) throws Exception
+    {
+        PQR newPqr = new PQR();
+
+        newPqr.setDescription(pqrDTO.getDescription());
+        newPqr.setUser(userService.getUserDataBase(pqrDTO.getIdCartUser()));
+        newPqr.setDateRequest(LocalDateTime.now());
+
+        return pqrRepo.save(newPqr).getId();
     }
 
     @Override
-    public List<PQRDTO> listPQR(int id) throws Exception {
-        return null;
+    public int updatePQR(int id, PQRDTO pqrdto) throws Exception
+    {
+        List<PQR> lstPQR = new ArrayList<>();
+        PQR pqr = getPQRBD(id);
+
+        pqr.setDescription(pqrdto.getDescription());
+        return pqrRepo.save(pqr).getId();
     }
 
     @Override
     public PQRGetDTO getPQR(int id) throws Exception {
-        return null;
+        return convertPQR(getPQRBD(id));
+    }
+
+    private PQR getPQRBD(int id) throws Exception {
+        PQR pqr = pqrRepo.getById(id);
+
+        if (pqr != null) {
+            throw new Exception("No existe la PQR con la id "+id);
+        }
+
+        return pqr;
+    }
+
+
+    private PQRGetDTO convertPQR(PQR pqr) {
+        PQRGetDTO pqrGetDTO = new PQRGetDTO(
+                pqr.getId(),
+                pqr.getDescription(),
+                pqr.getState(),
+                pqr.getDateRequest(),
+                pqr.getUser().getIdCard(),
+                pqr.getModerator().getIdCard());
+        return pqrGetDTO;
     }
 }
