8825b86501ed412f7b4ecb3e1b27cc42851071f0
Julian Quiroga <jaquirogab@uqvirtual.edu.co>
2023-04-25 16:05:57 -0500

    Update UserServiceTest.java
    Se Actualiza Test

--- uniMarket/src/test/java/co/edu/uniquindio/unimarket/test/UserServiceTest.java
+++ uniMarket/src/test/java/co/edu/uniquindio/unimarket/test/UserServiceTest.java
@@ -1,11 +1,19 @@
 package co.edu.uniquindio.unimarket.test;
 
-import co.edu.uniquindio.unimarket.services.UserService;
+import co.edu.uniquindio.unimarket.dto.ProductGetDTO;
+import co.edu.uniquindio.unimarket.dto.UserDTO;
+import co.edu.uniquindio.unimarket.dto.UserGetDTO;
+import co.edu.uniquindio.unimarket.model.entidad.State;
+import co.edu.uniquindio.unimarket.model.entidad.User;
+import co.edu.uniquindio.unimarket.services.interfaces.UserService;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+import java.util.ArrayList;
+import java.util.List;
 @SpringBootTest
 @Transactional
 public class UserServiceTest
@@ -14,19 +22,82 @@ public class UserServiceTest
     private UserService userService;
 
     @Test
-    public void register() throws Exception
+    @Sql("classpath:dataset.sql")
+    public void registerUserTest()
     {
-        UserPostDTO userPostDTO = new UserPostDTO(
+        try {
+            UserDTO userDTO = new UserDTO(
+                    "723",
+                    "JulianQ",
+                    "JulianQ@gmail.com",
+                    "1234",
+                    "Br montevideo Mz 6 casa 8",
+                    "3128190284");
 
-                "Pepito Perez",
-                "pepe@email.com",
-                "1234",
-                "Calle 12 # 1 - 5",
-                "7402324");
+            String idCard = userService.registerUser(userDTO);
 
-//Guardamos el registro usando el método del servicio
-        int new = userService.registerUser(userPostDTO);
-//Comprobamos que sí haya quedado guardado. Si se guardó debe retornar el código (no 0).
-        Assertions.assertNotEquals(0, new);
+            Assertions.assertNotEquals("", idCard);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
     }
-}
+    @Test
+    @Sql("classpath:dataset.sql")
+    public void updateUser()
+    {
+        try {
+            UserGetDTO User = userService.getUser("1094936382");
+            User.setName("JulianQ");
+
+            String idCardUpdate = userService.updateUser(User.getIdCard(), new UserDTO(
+                    User.getIdCard(),
+                    User.getName(),
+                    User.getEmail(),
+                    User.getNumPhone(),
+                    User.getUserName(),
+                    User.getPassword(),
+                    ));
+
+            User = userService.getUser(idCardUpdate);
+
+            Assertions.assertNotEquals("Julian Andres Quiroga Ballen", User.getName());
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        @Test
+        @Sql("classpath:dataset.sql")
+        public void getUser() {
+        try {
+            UserGetDTO UserGetDTO = userService.getUser("1094936382");
+
+            Assertions.assertEquals("JulianQB", UserGetDTO.getPassword());
+        } catch (Exception exception){
+            exception.printStackTrace();
+        }
+        @Test
+        @Sql("classpath:dataset.sql")
+        public void obtenerUsuarios() {
+            try {
+                List<UserGetDTO> lstUser = userService.getUser();
+
+                Assertions.assertNotEquals(0, listUser.size());
+            } catch (Exception exception){
+                exception.printStackTrace();
+            }
+        }
+        @Test
+        @Sql("classpath:dataset.sql")
+        public void changePassword()
+        {
+            try {
+                boolean answer = userService.changePassword("jaquirogab@uqvirtual.com", "1234");
+
+                Assertions.assertTrue(answer);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+    }
