0efefc417754c5a6c22d4e863c0753d0fa1f1313
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-04-24 23:25:36 -0500

    Update DetailCartService.java

--- uniMarket/src/main/java/co/edu/uniquindio/unimarket/services/interfaces/DetailCartService.java
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/services/interfaces/DetailCartService.java
@@ -1,11 +1,15 @@
 package co.edu.uniquindio.unimarket.services.interfaces;
 
+import co.edu.uniquindio.unimarket.dto.DetailCartDTO;
 import co.edu.uniquindio.unimarket.dto.DetailCartGetDTO;
 import co.edu.uniquindio.unimarket.model.DetailCart;
 
+import java.util.List;
+
 public interface DetailCartService
 {
-    DetailCartGetDTO getCartDetail(int id) throws Exception;
+    int createDetailCart(int id, DetailCartDTO detailCartDTO) throws Exception;
+    List<DetailCartGetDTO> getDetailsForCart(int id) throws Exception;
 
-    //DetailCart get(int id) throws Exception;
+    List<DetailCartGetDTO> convertLista(List<DetailCart> lstDetailCart);
 }
