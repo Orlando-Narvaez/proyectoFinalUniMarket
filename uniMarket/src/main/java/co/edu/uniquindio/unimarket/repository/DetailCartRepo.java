458ed7d5f62257775ca04ef39859703fd828f8f0
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-04-25 01:04:32 -0500

    Update DetailCartRepo.java

--- uniMarket/src/main/java/co/edu/uniquindio/unimarket/repository/DetailCartRepo.java
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/repository/DetailCartRepo.java
@@ -1,9 +1,14 @@
 package co.edu.uniquindio.unimarket.repository;
 
 import co.edu.uniquindio.unimarket.model.DetailCart;
-import org.hibernate.metamodel.model.convert.spi.JpaAttributeConverter;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import java.util.List;
 
-public interface DetailCartRepo extends JpaAttributeConverter<DetailCart, Integer>
+@Repository
+public interface DetailCartRepo extends JpaRepository<DetailCart, Integer>
 {
-
+    @Query("select d from DetailCart d WHERE d.cart.idCart = :idCart")
+    List<DetailCart> getDetailCartForCart(int idCart);
 }
