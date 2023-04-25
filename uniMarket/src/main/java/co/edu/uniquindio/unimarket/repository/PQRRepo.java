2705c1582dd9ecf55f1bbb606a106e956a8b3b4d
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-04-25 13:08:32 -0500

    Update PQRRepo.java

--- uniMarket/src/main/java/co/edu/uniquindio/unimarket/repository/PQRRepo.java
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/repository/PQRRepo.java
@@ -1,9 +1,16 @@
 package co.edu.uniquindio.unimarket.repository;
 
+import co.edu.uniquindio.unimarket.model.Comments;
 import co.edu.uniquindio.unimarket.model.PQR;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface PQRRepo extends JpaRepository<PQR, Integer>
 {
-
+    @Query("select p from PQR p where p.user.idCard = :idCard")
+    List<Comments> getPqrIdUser(int idCard);
 }
