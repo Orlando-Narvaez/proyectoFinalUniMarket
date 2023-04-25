c973199264ff08db40dd8a8f78aeee55f267b8db
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-04-25 00:16:25 -0500

    Update AddressService.java

--- uniMarket/src/main/java/co/edu/uniquindio/unimarket/services/interfaces/AddressService.java
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/services/interfaces/AddressService.java
@@ -1,14 +1,19 @@
 package co.edu.uniquindio.unimarket.services.interfaces;
 
+import co.edu.uniquindio.unimarket.dto.AddressDTO;
+import co.edu.uniquindio.unimarket.dto.AddressGetDTO;
+import co.edu.uniquindio.unimarket.model.Address;
+import java.util.List;
+
 public interface AddressService
 {
-    int crearDireccion(DireccionDTO direccionDTO)  throws Exception;
-    // Método que permite actualizar una dirección por medio de su código
-    int actualizarDireccion(int codigoDireccion, DireccionDTO direccionDTO) throws Exception;
-    // Método que permite eliminar una dirección de la base de datos por medio de se código
-    boolean eliminarDireccion(int codigoDireccion) throws Exception;
-    // Método que permite obtener las direcciones de un usuario por medio de su código
-    List<DireccionGetDTO> obtenerDireccionUsuario(String cedulaUsuario) throws Exception;
-    Direccion obtenerDireccionBD(int codigoDireccion) throws Exception;
-    DireccionGetDTO transformarDireccion(Direccion direccion);
+    int createAddress(AddressDTO addressDTO)  throws Exception;
+
+    int upsdateAddress(int id, AddressDTO addressDTO) throws Exception;
+
+    boolean delateAddress(int id) throws Exception;
+
+    List<AddressGetDTO> getAddressUser(String idCardUser) throws Exception;
+    Address getAddresBD(int id) throws Exception;
+    AddressGetDTO convertAddress(Address address);
 }
