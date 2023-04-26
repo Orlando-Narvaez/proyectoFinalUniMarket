f54989f434ca9e0489de13e6effd8fa3dd619b6c
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-04-25 23:15:49 -0500

    Update AddressServiceTest.java

--- uniMarket/src/test/java/co/edu/uniquindio/unimarket/test/AddressServiceTest.java
+++ uniMarket/src/test/java/co/edu/uniquindio/unimarket/test/AddressServiceTest.java
@@ -1,45 +1,80 @@
 package co.edu.uniquindio.unimarket.test;
 
-import co.edu.uniquindio.unimarket.dto.ProductDTO;
-import co.edu.uniquindio.unimarket.model.Categories;
-import co.edu.uniquindio.unimarket.services.interfaces.ProductService;
+import co.edu.uniquindio.unimarket.dto.AddressDTO;
+import co.edu.uniquindio.unimarket.dto.AddressGetDTO;
+import co.edu.uniquindio.unimarket.model.Address;
+import co.edu.uniquindio.unimarket.services.interfaces.AddressService;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
-import org.springframework.util.LinkedCaseInsensitiveMap;
-
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
+@SpringBootTest
+@Transactional
 public class AddressServiceTest
 {
     @Autowired
-    private ProductService productService;
+    private AddressService addressService;
 
     @Test
     @Sql("classpath:dataset.sql")
-    public void createProduct()
+    public void createAddress()
     {
         try {
-            Map<String,String> lstImages = new LinkedCaseInsensitiveMap<>();
-            lstImages.put("1", "https://www.wallpaperbetter.com/es/hd-wallpaper-asswj");
-            List<Categories> lstCategories = new ArrayList<>();
-            lstCategories.add(Categories.Belleza);
-            ProductDTO productDTO = new ProductDTO(
-                    "Producto de prueba",
-                    15000,
-                    "Pertenece el producto a un test",
-                    1,
-                    lstImages,
-                    lstCategories
-            );
+            int idAddress = addressService.createAddress(new AddressDTO(
+                    "La pavona",
+                    60003,
+                    1094978017
+            ));
 
-            int idProduct = productService.createProduct(productDTO);
+            Assertions.assertNotEquals(0, idAddress);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 
-            Assertions.assertNotEquals(0, idProduct);
+    @Test
+    @Sql("classpath:dataset.sql")
+    public void upsdateAddress()
+    {
+        try {
+            Address address = addressService.getAddresBD(1094978017);
+            address.setDescription("Villa Liliana");
+            int id = addressService.upsdateAddress(1094978017, new AddressDTO(
+                    address.getDescription(),
+                    address.getPostalCod(),
+                    address.getUser().getIdCard()
+            ));
 
+            Assertions.assertEquals(0, id);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @Test
+    @Sql("classpath:dataset.sql")
+    public void delateAddress()
+    {
+        try {
+            boolean answer = addressService.delateAddress(1094978017);
+
+            Assertions.assertNotEquals(true, answer);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+    @Test
+    @Sql("classpath:dataset.sql")
+    public void getAddressUser()
+    {
+        try {
+            List<AddressGetDTO> lstAddress = addressService.getAddressUser("1094978017");
+
+            Assertions.assertNotEquals(0, lstAddress.size());
         } catch (Exception ex) {
             ex.printStackTrace();
         }
