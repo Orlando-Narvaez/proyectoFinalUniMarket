2e0fb76284fab958298ae1d99e327b26fdf86bc1
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-04-25 19:10:44 -0500

    Update GuaranteeRepo.java

--- uniMarket/src/main/java/co/edu/uniquindio/unimarket/repository/GuaranteeRepo.java
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/repository/GuaranteeRepo.java
@@ -2,8 +2,13 @@ package co.edu.uniquindio.unimarket.repository;
 
 import co.edu.uniquindio.unimarket.model.Guarantee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import java.util.List;
 
+@Repository
 public interface GuaranteeRepo extends JpaRepository<Guarantee,Integer>
 {
-
+    @Query("select g from Guarantee g where g.user.idCard = :idCard")
+    List<Guarantee> getGuaranteeUser(int idCard);
 }
