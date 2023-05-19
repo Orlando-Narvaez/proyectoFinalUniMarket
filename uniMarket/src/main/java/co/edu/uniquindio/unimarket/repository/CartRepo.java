566cce64ae752a3069414dbf702ddfe57bbd6e21
Orlando Narvaez <orlandonarvaezb@gmail.com>
2023-05-18 22:53:30 -0500

    Update CartRepo.java

--- uniMarket/src/main/java/co/edu/uniquindio/unimarket/repository/CartRepo.java
+++ uniMarket/src/main/java/co/edu/uniquindio/unimarket/repository/CartRepo.java
@@ -1,6 +1,7 @@
 package co.edu.uniquindio.unimarket.repository;
 
 import co.edu.uniquindio.unimarket.model.Cart;
+import co.edu.uniquindio.unimarket.model.StateCart;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -11,4 +12,7 @@ public interface CartRepo extends JpaRepository<Cart, Integer>
 {
     @Query("select c from Cart c where c.user.idCard = :idCard")
     List<Cart> getLstCartForUser(int idCard);
+
+    @Query("select c from Cart c where c.stateCart = :stateCart")
+    List<Cart> getLstCartForState(StateCart stateCart);
 }
